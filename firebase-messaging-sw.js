72ed54b4ecdac33369483a9041906b099ff68db9
thefinancialcraft <thefinancialcraft.com>
2025-04-05 19:13:04 +0530

    ok

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBcbc5HEqoB3T6iXFsuKu2l25FJEgJSwGw",
+  authDomain: "tfc-connect.firebaseapp.com",
+  projectId: "tfc-connect",
+  storageBucket: "tfc-connect.firebasestorage.app",
+  messagingSenderId: "259719401882",
+  appId: "1:259719401882:web:9d1c8418e7baea37ff2a23",
+  measurementId: "G-L6NX6YYCNB"
 });
 
 const messaging = firebase.messaging();
